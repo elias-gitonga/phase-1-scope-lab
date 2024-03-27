2384f998bba60da9903a7abe3d23e99bbe436d61
elias <elias.g.munene@gmail.com>
2024-03-27 14:30:09 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'; 
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'; 
+}
+
+
+const leastFavoriteCustomer = 'someone';
+
+
+function changeLeastFavoriteCustomer() {
+    
+    leastFavoriteCustomer = 'someone else'; 
+}
+
+console.log(upperCaseCustomerName());
+console.log(bestCustomer); 
+
+overwriteBestCustomer(); 
+console.log(bestCustomer); 
+
+console.log(leastFavoriteCustomer); 
+changeLeastFavoriteCustomer(); 
